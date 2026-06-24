1dabf32c9c0cdcbd5cc86d90030f38fe4c0f17c7
felipeab-b <felipeabferreira22@gmail.com>
2026-06-23 21:36:55 -0300

    feat: simulador rodando as threads

---
+++ simulador.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+from transmissor import iniciar_tx
+from receptor import iniciar_rx
+
+def simular(texto, mod, enq, edc, callback):
+    sock_tx, sock_rx = socket.socketpair()
+
+    thread_tx = threading.Thread(
+        target=iniciar_tx,
+        args=(sock_tx, texto, mod, enq, edc)
+    )
+    thread_rx = threading.Thread(
+        target=iniciar_rx,
+        args=(sock_rx, mod, enq, edc, callback)
+    )
+
+    thread_tx.start()
+    thread_rx.start()
